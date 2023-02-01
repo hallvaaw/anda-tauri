f2b1f955274db12ba4566a45209b7eafd66ed991
hallvaaw <hallvard_wahler@hotmail.com>
2023-02-01 15:47:55 +0100

    added fs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,12 @@
 
 fn main() {
 tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![my_custom_command])
    .run(tauri::generate_context!())
    .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
-   }
+fn my_custom_command(invoke_message: String) {
+    println!("I was invoked from JS, with this message: {}", invoke_message);
+}
